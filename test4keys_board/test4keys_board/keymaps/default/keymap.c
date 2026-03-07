605c305f0901767ca51161bc26d65677b060d750
albinsibi365 <albinsibi365@gmail.com>
2026-03-06 20:38:50 -0800

    changed the keymap.c to try and display a red square, changed the divisor value to 16

--- test4keys_board/test4keys_board/keymaps/default/keymap.c
+++ test4keys_board/test4keys_board/keymaps/default/keymap.c
@@ -1,6 +1,6 @@
 // Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
-#include "shiheart.qgf.h"
+//#include "shiheart.qgf.h"
 #include QMK_KEYBOARD_H
 #include <qp.h>
 #include <spi_master.h>
@@ -8,11 +8,11 @@
 #define TFT_CS_PIN    A2   // Chip Select
 #define TFT_DC_PIN    A4   // Data/Command
 #define TFT_RST_PIN   A3   // Reset
-#define LCD_SPI_DIVISOR 4
+#define LCD_SPI_DIVISOR 16
 
 
 static painter_device_t display;
-static painter_image_handle_t shiheart;
+//static painter_image_handle_t shiheart;
 
 void keyboard_post_init_user(void) {
     display = qp_ili9341_make_spi_device(
@@ -26,10 +26,12 @@ void keyboard_post_init_user(void) {
     );
 
     qp_init(display, QP_ROTATION_0);
-    shiheart = qp_load_image_mem(gfx_shiheart);
-     if (shiheart != NULL) {
-        qp_drawimage(display, 0, 0, shiheart);
-    }
+    qp_rect(display, 0, 0, 240, 320, 255, 0, 0, true);
+
+  //  shiheart = qp_load_image_mem(gfx_shiheart);
+  //   if (shiheart != NULL) {
+  //      qp_drawimage(display, 0, 0, shiheart);
+  //  }
 }
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
