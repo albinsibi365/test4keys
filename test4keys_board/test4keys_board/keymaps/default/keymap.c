1fe5d5ae0219cd7ed9c5748b9afb57e65f09c432
albinsibi365 <albinsibi365@gmail.com>
2026-03-08 21:37:16 -0700

    updated to include logo

--- test4keys_board/test4keys_board/keymaps/default/keymap.c
+++ test4keys_board/test4keys_board/keymaps/default/keymap.c
@@ -1,6 +1,7 @@
 // Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include "shiheartcolor.qgf.h"
+#include "atrilogo240x320.qgf.h"
 #include QMK_KEYBOARD_H
 #include <qp.h>
 #include <spi_master.h>
@@ -12,6 +13,7 @@
 
 
 static painter_device_t display;
+static painter_image_handle_t atrilogo240x320;
 static painter_image_handle_t shiheartcolor;
 
 
@@ -29,7 +31,25 @@ void keyboard_post_init_user(void) {
 
     qp_init(display, QP_ROTATION_0);
     qp_clear(display);
+    //start of add
+    
+    atrilogo240x320 = qp_load_image_mem(gfx_atrilogo240x320);
+    qp_drawimage(
+        display,
+        (240 - atrilogo240x320->width) / 2,
+        (320 - atrilogo240x320->height) / 2,
+        atrilogo240x320
+    );
+    qp_flush(display);
+    wait_ms(2000);  // 2 seconds
 
+    
+    for (int i = 0; i < 10; i++) {
+        qp_rect(display, 0, 0, 240, 320, i * 10, i * 10, i * 10, true);
+        wait_ms(20);
+    }
+
+    //end of add
     shiheartcolor = qp_load_image_mem(gfx_shiheartcolor);
      if (shiheartcolor != NULL) {
         qp_drawimage(display, 0, 0, shiheartcolor);
--- test4keys_board/test4keys_board/rules.mk
+++ test4keys_board/test4keys_board/rules.mk
@@ -1,3 +1,4 @@
 SRC += shiheartcolor.qgf.c
+SRC += atrilogo240x320.qgf.c
 QUANTUM_PAINTER_ENABLE = yes
 QUANTUM_PAINTER_DRIVERS += ili9341_spi
